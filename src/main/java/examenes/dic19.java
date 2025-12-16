8c44432a5e340e25be16499093b25da37a677493
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-12-16 12:50:27 +0100

    s

--- src/main/java/examenes/dic19.java
+++ src/main/java/examenes/dic19.java
@@ -1,5 +1,9 @@
 package examenes;
 
 public class dic19 {
-
+	@WebServlet(“/contador”)
+	public class ServletContador extends HttpServlet {
+	protected void doGet _(HttpServletRequest request, HttpServletResponse
+	response) throws ServletException, IOException {
+	 InputStream file = getServletContext().getResourceAsStream("/contador.html");
 }

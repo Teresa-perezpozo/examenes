967b5c8146faa5381abb68285b0bc9eb2fa95d61
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-12-16 11:52:53 +0100

    fa

--- src/main/java/examenes/comprobadorEdad.java
+++ src/main/java/examenes/comprobadorEdad.java
@@ -1,4 +1,10 @@
 package examenes;
+
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @WebServlet("index/html")
 public class comprobadorEdad extends HttpServlet{
 //doGet
@@ -8,4 +14,18 @@ response.setContentType("text/html");
 PrintWriter out = response.getWriter();
 if(edad<18) out.print("<html><body>menor</body></html>")
 else out.print("<html><body>mayor</body></html>")
+out.close();
+}
+//EXÁMEN 17-18
+protected void doPost _(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String dni = request.getParameter("dni");
+	
+	PrintWriter out = resoponse.getWriter();
+	response.setContentType("text/html");
+
+	if(sesion.findByDni("dni") == null) out.print("<html><body>No está registrado");
+	else out.print("el paciente ya se encuentra registrado");
+	out.close();
+
+
 }
---
+++ src/main/java/examenes/ex1819.java
@@ -0,0 +1,39 @@
+package examenes;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("login.html")
+public class ex1819 {
+	public class Login extends HttpServlet {
+		 protected void doPost (HttpServletRequest request, HttpServletResponse response){
+			 String usuario = request.getParameter("usuario");
+			 String password = (String)request.getParameter("password");
+			 PrintWriter out = response.getWriter();
+			 HttpSession sesion = request.getSession();
+			 response.setContentType("text/html");
+
+			 if(sesion.getAtribute("user").equals(password)) {
+			 out.print("<html><body>autennticación correcta</body></html>");
+			 }else {
+			 out.print("<html><body>autennticación incorrecta</body></html>");
+			 
+		 }
+			 out.close();			 
+			 }
+
+}
+	//ejercicio 3
+	public static void main(String[] args) throws Exception {
+		Socket conexión;
+		try {
+			conexión= new Socket("", 10007);
+		}catch{}
+		InputStream in = conexión.getInputStream();
+		OutputStream out = conexión.getOutputStream();
+		}
+	}
+}
